923c36b9380e72ea17a15959041aadfc9e1745ea
arafat22184 <123alarafat@gmail.com>
2025-02-26 08:35:45 +0600

    added logout event

--- script/login.js
+++ script/login.js
@@ -2,12 +2,14 @@ document.getElementById("login-btn")
   .addEventListener('click', function (event) {
     event.preventDefault();
     const accountNumber = document.getElementById("account-number").value;
+    const convertedAccountNumber = parseInt(accountNumber);
     const pin = document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
-    if (accountNumber.length === 11) {
+    console.log(typeof accountNumber);
+    if (convertedAccountNumber === 11111000134) {
 
-      if (convertedPin === 1234) {
-        window.location.href="./main.html"
+      if (convertedPin === 123456) {
+        window.location.href = "./main.html"
       }
       else {
         alert("Invalid Pin");
@@ -16,6 +18,9 @@ document.getElementById("login-btn")
     else {
       alert("need valid account number");
     }
+  });
+
+document.getElementById('forgot-pin').addEventListener('click', function () {
+    alert(`please contact Admin`)
   })
 
-  
